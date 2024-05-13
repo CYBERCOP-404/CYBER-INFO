eb945fce4522a16d0d6f9dc36d8617f907496d7d
CYBERCOP-404 <168484308+CYBERCOP-404@users.noreply.github.com>
2024-05-14 02:51:27 +0600

    main.py

---
+++ main.py
@@ -0,0 +1,38 @@
+# import section Start
+import os 
+from time import sleep
+# import section end
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
+BANNER='''\033[1;32m
+  /$$$$$$  /$$     /$$ /$$$$$$$  /$$$$$$$$ /$$$$$$$           /$$$$$$   /$$$$$$  /$$$$$$$ 
+ /$$__  $$|  $$   /$$/| $$__  $$| $$_____/| $$__  $$         /$$__  $$ /$$__  $$| $$__  $$
+| $$  \__/ \  $$ /$$/ | $$  \ $$| $$      | $$  \ $$        | $$  \__/| $$  \ $$| $$  \ $$
+| $$        \  $$$$/  | $$$$$$$ | $$$$$   | $$$$$$$/ /$$$$$$| $$      | $$  | $$| $$$$$$$/
+| $$         \  $$/   | $$__  $$| $$__/   | $$__  $$|______/| $$      | $$  | $$| $$____/ 
+| $$    $$    | $$    | $$  \ $$| $$      | $$  \ $$        | $$    $$| $$  | $$| $$      
+|  $$$$$$/    | $$    | $$$$$$$/| $$$$$$$$| $$  | $$        |  $$$$$$/|  $$$$$$/| $$      
+ \______/     |__/    |_______/ |________/|__/  |__/         \______/  \______/ |__/   Verson-2.1
+'''
+command_list='''
+[1] PYTHON COLOR CODE  
+'''
+comm ='''\033[0;31m
+LOGIN ERROR ....
+'''
+while True:
+    clear_screen()
+    print(BANNER)
+    print(command_list)
+    CHOICE = input('\033[1;34mENTER YOUR CHOICE : ')
+    if CHOICE =='1':
+        os.system('python-color-code.txt')
+    else:
+        for i in range(10,0,-1):
+            sleep(0.5)
+            clear_screen()
+            print(BANNER)
+            print(comm)
+            print(f'TRY AFTER {i} SECOND')
+            sleep(0.5)
+# Developed by CYBER COP BANGLADESH
