eca50d52eeb0a51b88b7371e6de64d9420e75dc1
CYBER-COP-BANGLADESH <168484308+CYBERCOP-404@users.noreply.github.com>
2024-05-19 19:19:01 +0600

    Create fb_report.py

---
+++ fb_report.py
@@ -0,0 +1,62 @@
+BANNER ='''\033[0;36m
+	  ______ ____        _____  ______ _____   ____  _____ _______ 
+	 |  ____|  _ \      |  __ \|  ____|  __ \ / __ \|  __ \__   __|
+	 | |__  | |_) |_____| |__) | |__  | |__) | |  | | |__) | | |   
+	 |  __| |  _ <______|  _  /|  __| |  ___/| |  | |  _  /  | |   
+	 | |    | |_) |     | | \ \| |____| |    | |__| | | \ \  | |   
+	 |_|    |____/      |_|  \_\______|_|     \____/|_|  \_\ |_|   
+ '''
+ADMIN_LIST ='''
+\033[1;39m     ┏━━━━━━━━━━━━━━━━━━━\033[38;5;46m𝗞𝗜𝗡𝗚\033[1;39m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+\033[1;39m     ┃ \x1b[1;95m❣︎[𖣘]☔︎\x1b[1;96m𝙉𝘼𝙈𝙀\033[1;34m              : [★]  CYBER COP BANGLADESH\033[1;39m  ┃
+\033[1;39m     ┃ \x1b[1;95m❣︎[𖣘]☔︎\x1b[1;96m𝙁𝘼𝘾𝙀𝘽𝙊𝙊𝙆\033[1;34m      : [★]  CYBER COP\033[1;39m             ┃
+\033[1;39m     ┃ \x1b[1;95m❣︎[𖣘]☔︎\x1b[1;96m𝙂𝙄𝙏𝙃𝙐𝘽\033[1;34m          : [★]  CYBERCOP-404\033[1;39m          ┃
+\033[1;39m     ┃ \x1b[1;95m❣︎[𖣘]☔︎\x1b[1;96m𝙍𝙄𝙇𝙄𝙂𝙀𝙎𝙃𝙊𝙉\033[1;34m  : [★]  𝗕𝗔𝗡𝗚𝗟𝗔𝗗𝗘𝗦𝗛𝗜\033[1;39m┃
+\033[1;39m     ┃ \x1b[1;95m❣︎[𖣘]☔︎\x1b[1;96m𝙒𝙃𝘼𝙏𝙎𝘼𝙋𝙋\033[1;34m      : [★]  +8809638223345\033[1;39m        ┃
+\033[1;39m     ┃ \x1b[1;95m❣︎[𖣘]☔︎\x1b[1;96m𝙏𝙊𝙊𝙇𝙎 𝙉𝘼𝙈𝙀\033[1;34m   : [★]  FACEBOOK-AUTO-REPORT\033[1;39m  ┃
+ \033[1;39m    ┗━━━━━━━━━━━━━━━━━━━\033[1;31m𝙁𝙄𝙍𝙀\033[1;39m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+'''
+#  IMPORT SECTION HERE 
+import os
+from time import sleep
+from random import choice
+#  IMPORT SECTION END 
+#  URL HERE
+FACEBOOK = "https://www.facebook.com/cybercopbangladesh"
+YOUTUBE = "https://www.youtube.com/cybercopbangladesh"
+github = "https://www.github.com/cybercop-404"
+telegram = "https://t.me/cybercopbangladesh"
+#  URL END HERE 
+R = '\033[31;1m'
+G = '\033[32;1m'
+Y = '\033[33;1m'
+B = '\033[34;1m'
+M = '\033[35;1m'
+C = '\033[36;1m'
+LR = '\033[91;1m'
+LG = '\033[92;1m'
+LY = '\033[93;1m'
+LB = '\033[94;1m'
+LM = '\033[95;1m'
+LC = '\033[96;1m'
+os.system(f'xdg-open {FACEBOOK}')
+sleep(4)
+os.system(f'xdg-open {YOUTUBE}')
+sleep(4)
+os.system(f'xdg-open {github}')
+sleep(4)
+os.system(f'xdg-open {telegram}')
+sleep(4)
+while True:
+    os.system('clear')
+    print(BANNER)
+    print(ADMIN_LIST)
+    print('PRESS ENTER TO RUN THE TOOL')
+    id_link =int(input('ENTER YOUR TARGET ID : '))
+    repost =int(input('HOW MANY REPORT DO YOU WANT TO SENT (MAX 100): '))
+    for i in range(repost):
+        dc = choice([R,G,Y,B,M,C,LR,LG,LY,LB,LM])
+        print(dc+'REPORT SUCESS 😈 ')
+        sleep(0.4)
+       
+# Developer CYBER-COP-BANGLADESH
