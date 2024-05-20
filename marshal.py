41aa5780e37a903a643b8f36a82b55cba9b3957e
CYBER-COP <168484308+CYBERCOP-404@users.noreply.github.com>
2024-05-20 14:19:21 +0600

    marshal.py

---
+++ marshal.py
@@ -0,0 +1,74 @@
+#-----------------INFO----------------#
+#CODING CYBER-COP
+#FACEBOOK > CYBER-COP 
+#GITHUB > CYBERCOP-404
+#Join Telegram Channel For More Cracking Stuffs
+#https://t.me/cybercopbangladesh
+## https://t.me/cybercopbangladesh1
+#-------------MAIN>MENU-------------#
+
+
+import marshal,os,sys,time
+os.system("clear")
+os.system("xdg-open https://github.com/cybercop-404")
+os.system("xdg-open https://github.com/cybercop-404")
+os.system("clear")
+os.system("xdg-open https://t.me/cybercopbangladesh")
+
+    
+logo = ("""\033[1;92m
+
+        $$$$$$$$\ $$\   $$\  $$$$$$\  
+        $$  _____|$$$\  $$ |$$  __$$\ 
+        $$ |      $$$$\ $$ |$$ /  \__|
+        $$$$$\    $$ $$\$$ |$$ |      
+        $$  __|   $$ \$$$$ |$$ |      
+        $$ |      $$ |\$$$ |$$ |  $$\ 
+        $$$$$$$$\ $$ | \$$ |\$$$$$$  |
+        \________|\__|  \__| \______/                                                                                           
+\033[1;39m     ┏━━━━━━━━━━━━━━━━━━━\033[38;5;46m𝗞𝗜𝗡𝗚\033[1;39m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+\033[1;39m     ┃ \x1b[1;95m❣︎[𖣘]☔︎\x1b[1;96m𝙉𝘼𝙈𝙀\033[1;34m              : [★]  CYBER COP BANGLADESH\033[1;39m  ┃
+\033[1;39m     ┃ \x1b[1;95m❣︎[𖣘]☔︎\x1b[1;96m𝙁𝘼𝘾𝙀𝘽𝙊𝙊𝙆\033[1;34m      : [★]  CYBER COP\033[1;39m             ┃
+\033[1;39m     ┃ \x1b[1;95m❣︎[𖣘]☔︎\x1b[1;96m𝙂𝙄𝙏𝙃𝙐𝘽\033[1;34m          : [★]  CYBERCOP-404\033[1;39m          ┃
+\033[1;39m     ┃ \x1b[1;95m❣︎[𖣘]☔︎\x1b[1;96m𝙍𝙄𝙇𝙄𝙂𝙀𝙎𝙃𝙊𝙉\033[1;34m  : [★]  𝗕𝗔𝗡𝗚𝗟𝗔𝗗𝗘𝗦𝗛𝗜\033[1;39m┃
+\033[1;39m     ┃ \x1b[1;95m❣︎[𖣘]☔︎\x1b[1;96m𝙒𝙃𝘼𝙏𝙎𝘼𝙋𝙋\033[1;34m      : [★]  +8809638223345\033[1;39m        ┃
+\033[1;39m     ┃ \x1b[1;95m❣︎[𖣘]☔︎\x1b[1;96m𝙏𝙊𝙊𝙇𝙎 𝙉𝘼𝙈𝙀\033[1;31m   : [★]  MARSHAL-CYTHON-TOOL\033[1;39m  ┃
+ \033[1;39m    ┗━━━━━━━━━━━━━━━━━━━\033[1;31m𝙁𝙄𝙍𝙀\033[1;39m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+""")
+def main():
+        try:
+        	   
+                print (logo)
+                print ('    \033[1;32m┏━━━\033[97;1m\033[1;31m[\x1b[1;92m+\033[1;31m]\033[1;32m EXAMPLE :\033[33;1m/sdcard/file.py')
+                file = input ('    \033[1;32m┗━━━\033[97;1m\033[1;31m[\x1b[1;92m+\033[1;31m] \033[1;32mFILE NAME :\033[33;1m ')
+                fileopen = open(file).read()
+                a = compile(fileopen, 'dg', 'exec')
+                m = marshal.dumps(a)
+                s = repr(m)
+                b = ('##-----------------------ADMIN>INFO---------------------------##\n# ENCRYPTION BY :CYBER-COP\n# VERSION : 1.0.2\n# GITHUB : https://github.com/CYBERCOP-404\n##------------------------MAIN>MENU-------------------------##\nimport marshal\nexec(marshal.loads(' + s +'))')
+                c = file.replace('.py', '.py')
+                d = open(c, 'w')
+                d.write(b)
+                d.close()
+                print ('    \033[97;1m\033[1;31m[\x1b[1;92m+\033[1;31m] \033[1;32mENCRYPTION SUCCESSFUL >',c)
+                print('    \033[1;32m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━')
+                time.sleep(1)
+                aq = input ('    \033[97;1m\033[1;31m[\x1b[1;92m+\033[1;31m] \033[1;32mWANT TO ENCRYPT AGAIN? [Y/N]')
+                if aq =="":
+                        print ('    \033[97;1m\033[1;31m[\x1b[1;92m+\033[1;31m] \033[1;32mCOMMAND NOT FOUND !')
+                        os.sys.exit()
+                elif aq =="y" or aq =="Y":
+                        main()
+                else:
+                        if aq =="n" or aq =="N":
+                                print ('    \033[97;1m\033[1;31m[\x1b[1;92m+\033[1;31m] \033[1;32m➤THANK YOU BRO ;v\n')
+                        else:
+                                print ('    \033[97;1m\033[1;31m[\x1b[1;92m+\033[1;31m] \033[1;32mCOMMAND NOT FOUND!')
+                                os.sys.exit()
+        except IOError:
+                print ('   \033[97;1m\033[1;31m[\x1b[1;92m+\033[1;31m] \033[1;32mFILE NOT FOUND ! ')
+                time.sleep(1)
+                main()
+
+if __name__=='__main__':
+        main()
